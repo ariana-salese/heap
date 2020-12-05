14a35bcf4000dd872f2a32a8acae24aefdd6d219
ariana-salese <71737227+ariana-salese@users.noreply.github.com>
2020-12-05 16:55:02 -0300

    Add files via upload

---
+++ pruebas_heap.c
@@ -0,0 +1,33 @@
+#define _POSIX_C_SOURCE 200809L
+#include "testing.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define CANTIDAD_ELEMENTOS 15
+
+/* ******************************************************************
+ *                 NUESTRAS PRUEBAS (ELIMINAR)
+ * *****************************************************************/
+
+
+/* ******************************************************************
+ *                 PRUEBAS PRIMITIVAS HEAP
+ * *****************************************************************/
+
+void nuestras_pruebas() {
+
+}
+
+void pruebas_heap_estudiante() {
+    nuestras_pruebas();
+}
+
+//#ifndef CORRECTOR
+
+int main(void) {
+    pruebas_heap_estudiante();
+    return failure_count() > 0;
+}
+
+//#endif
